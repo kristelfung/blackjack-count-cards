b80b17a4f0e1dcf10d168a25420a2e69b4dcc13c
Kristel Fung <kristelfung@gmail.com>
2020-12-13 15:04:09 +0800

    Start screen grid

--- App.py
+++ App.py
@@ -7,18 +7,55 @@ from Table import Table
 class App(tk.Frame):
   def __init__(self, master=None):
     super(App, self).__init__(master)
-    self.initGame()
+    self.create_window()
   
-  def initGame(self):
-    """ Create Start and Quit button. """
-    self.start_button = tk.Button(self, text="Start (S)", command=self.start)
-    self.start_button.pack()
-    self.quit_button = tk.Button(self, text="Quit (Q)", command=self.quit)
-    self.quit_button.pack()
+  def create_window(self):
+    """ Create grid and start/quit buttons. """
+    # Add padding to the entire window
+    self.master['padx'] = 5
+    self.master['pady'] = 5
+    
+    # Create [0, 0] Count Frame
+    self.count_frame = tk.Frame(self, width=200, height=120)
+    self.count_frame.grid(row=0, column=0, padx=5, pady=5)
+    
+    # Create [0, 1] Dealer Frame
+    self.dealer_frame = tk.Frame(self, width=200, height=120)
+    self.dealer_frame.grid(row=0, column=1, padx=5, pady=5)
+    
+    # Create [0, 2] Quit Frame
+    self.quit_frame = tk.Frame(self, width=200, height=120)
+    self.quit_frame.grid(row=0, column=2, padx=5, pady=5, sticky=tk.N + tk.E)
+    
+    # Create [1, 0] Money Frame
+    self.money_frame = tk.Frame(self, width=200, height=120)
+    self.money_frame.grid(row=1, column=0, padx=5, pady=5)
+    
+    # Create [1, 1] Action Frame
+    self.action_frame = tk.Frame(self, width=200, height=120)
+    self.start_button = tk.Button(self.action_frame, text="Start (S)", command=self.start)
+    self.start_button.grid(row=0, column=0)
+    self.quit_button = tk.Button(self.action_frame, text="Quit (Q)", command=self.quit)
+    self.quit_button.grid(row=1, column=0)
+    self.action_frame.grid(row=1, column=1, padx=5, pady=5)
+    
+    # Create [1, 2] Table Frame
+    self.table_frame = tk.Frame(self, width=200, height=120)
+    self.table_frame.grid(row=1, column=2, padx=5, pady=5)
+    
+    # Create a [2, 1] Player Frame
+    self.player_frame = tk.Frame(self, width=200, height=120)
+    self.player_frame.grid(row=2, column=1, padx=5, pady=5)
   
   def start(self):
     """ Begins game. Initializes Table with 4 decks, Player, and Dealer. """
-    self.start_button.destroy()
+    # Clear action frame and put quit button in quit_frame
+    for widget in self.action_frame.winfo_children():
+      widget.destroy()
+    
+    self.quit_button = tk.Button(self.quit_frame, text="Quit (Q)", command=self.quit)
+    self.quit_button.grid(row=0, column=0)
+    
     self.table = Table(num_decks=4)
     self.player = Player(self.master)
     self.dealer = Dealer(self.master)
