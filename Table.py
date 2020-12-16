02a65d7c7103f80bc4dc567a3de8909898a45da4
Kristel Fung <kristelfung@gmail.com>
2020-12-17 01:11:24 +0800

    Shoe label

--- Table.py
+++ Table.py
@@ -37,7 +37,12 @@ class Table:
     self.label_rc.grid(row=0, column=0)
     self.label_tc = tk.Label(self.parent.status_frame, text="True Count is " + str(self.count / 4))
     self.label_tc.grid(row=1, column=0)
-
+    
+    self.parent.table_frame.grid_columnconfigure((0), weight=1)
+    self.parent.table_frame.grid_rowconfigure((0), weight=1)
+    self.label_deck = tk.Label(self.parent.table_frame, text="Shoe (" + str(len(self.shoe)) + ")")
+    self.label_deck.grid(row=0, column=0)
+    
   def shuffle(self):
     """Shuffles the shoe."""
     random.shuffle(self.shoe)
@@ -48,12 +53,14 @@ class Table:
     player.init_cards([self.shoe.pop(), self.shoe.pop()])
     self.update_count(player.hand.cards)
     self.update_count([dealer.hand.cards[0]])
+    self.update_shoe_label()
 
   def deal_one_card(self, hand):
     """Deals one card (used for Player and Dealer hit)."""
     card_hand = [self.shoe.pop()]
     hand.receive_cards(card_hand)
     self.update_count(card_hand)
+    self.update_shoe_label()
 
   def update_count(self, cards):
     """Updates count of the Table based on cards and updates count Labels."""
@@ -80,3 +87,6 @@ class Table:
         self.shoe += d.cards
       self.shuffle()
       self.update_count([])
+  
+  def update_shoe_label(self):
+    self.label_deck.config(text="Shoe (" + str(len(self.shoe)) + ")")
